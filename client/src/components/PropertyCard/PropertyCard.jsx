70e74eceda3c60c57587254be9aa242fb81673a0
sahan madusha <sahanmadusha001@gmail.com>
2025-01-05 16:25:31 +0530

    single page UI rewamp

--- client/src/components/PropertyCard/PropertyCard.jsx
+++ client/src/components/PropertyCard/PropertyCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { SINGLEVIEW } from "../../constant/AppConstant.js";
 import { useWishlist } from "../../Context/WishlistContext.jsx";
 
-export const PropertyCard = ({ property }) => {
+export const PropertyCard = ({ property, isViewDetaiShow = true }) => {
   const { addToWishlist, removeFromWishlist, isInWishlist } = useWishlist();
 
   const handleWishlistClick = () => {
@@ -31,12 +31,14 @@ export const PropertyCard = ({ property }) => {
           </p>
           <p className="small text-muted mb-2">{property?.shortdescription}</p>
           <div className="d-flex justify-content-between align-items-center">
-            <Link
-              to={`../${SINGLEVIEW}/${property?.id}`}
-              className="btn btn-outline-primary btn-sm"
-            >
-              View Details
-            </Link>
+            {isViewDetaiShow && (
+              <Link
+                to={`../${SINGLEVIEW}/${property?.id}`}
+                className="btn btn-outline-primary btn-sm"
+              >
+                View Details
+              </Link>
+            )}
             <button
               className={`btn btn-sm wishlist-btn rounded-circle py-1 px-2 ${
                 isInWishlist(property?.id)
